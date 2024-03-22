276b2e3b8fb19d9dd5ad80635401105df9f3c3a1
Kevin Tran <kthightran024@gmail.com>
2024-03-21 19:21:08 -0700

    updated BEM naming and added JS for active__nav__link

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const navLinkEls = document.querySelectorAll('.nav__link');
 const windowPathName = window.location.pathname;
 
-navLinkEls forEach 
+navLinkEls.forEach(navLinkEl => {
+  const navLinkPathname = new URL(navLinkEl.href).pathname;
+  
+  if (
+    (windowPathName === navLinkPathname) || 
+    (windowPathName === '/index.html' && navLinkPathname === '/')
+    ) {
+    navLinkEl.classList.add('active__nav__link');
+  }
+});
